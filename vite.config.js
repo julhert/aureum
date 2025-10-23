acc89bc8d9098acc0d5b64d7a6386ced4dffdc1e
Julian H. Trejo <ljht.2004@gmail.com>
2025-10-22 19:15:26 -0600

    Modificaciones al archivo de configuración de Vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
     plugins: [
         tailwindcss()
     ],
     base: process.env.NODE_ENV === 'production' ? '/aureum/' : '/',
+    build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        colecciones: path.resolve(__dirname, 'colecciones.html'),
+        visitar: path.resolve(__dirname, 'visitar.html'),
+      }
+    }
+  }
 })
